406cda313ffff3d216a08a74dbf1114800b65402
roy-santos <roysantos112@gmail.com>
2020-07-27 22:22:25 -0700

    Add structure_dump and Application_Screeshots folders. Updated README.md file.

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-"# C195_Software2_PA_Roy_Santos" 
+#CRM Software
+A customer relationship management application for a fictitious company called Global Consulting Services built using Java and JavaFX (with Scenebuilder).
+
+### Installation
+**Note**: This application is only for educational purposes.  It will attempt to connect to a local host.
+ If trying to run the build on your local machine, copy the database schemas in the structures_dump
+ folder.
+ 1. Clone the repository
+ 2. Use the structures_dump files to replicate the database on your local machine.
+ 3. Modify the DBCConnection file to connect to your DB.
+ 4. Run the Main.java file. (Create a username and pass in the user table in your local DB server and login using those credentials).
+
+ ### Features
+ * Full CRM application that has a login page. (login is for demonstration purposes, credentials are not encrypted).
+ * Allows for each user to create their own customers and appointments.
+ * Alerts and Error messages when user violates DBMS rules.
+
+### Project Takeaways
+**Establishing DB Connection**: Using Java's mySQL JDBC API to connect to database.
+
+**DAO Design Pattern**: Utilizes data access object(DAO) design pattern to abstract database related code from
+application code.
+
+**Application Security**: Implementing a login page to validate an authorized user of the connected database.
+
+### Images
+**Login screen**:
+<img src="Application_Screenshots/GCSlogin.png" width=300 align=center>
+
+**Dashboard**:
+<img src="Application_Screenshots/GCSmain.png" width=300 align=center>
+
+**Customers screen**:
+<img src="Application_Screenshots/GCScustomers.png" width=300 align=center>
+
+**Appointments screen**:
+<img src="Application_Screenshots/GCSappointments.png" width=300 align=center>
+
---
+++ structures_dump/U06L3C_city.sql
@@ -0,0 +1,48 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: 3.227.166.251    Database: U06L3C
+-- ------------------------------------------------------
+-- Server version	5.7.30-0ubuntu0.18.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `city`
+--
+
+DROP TABLE IF EXISTS `city`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `city` (
+  `cityId` int(10) NOT NULL AUTO_INCREMENT,
+  `city` varchar(50) NOT NULL,
+  `countryId` int(10) NOT NULL,
+  `createDate` datetime NOT NULL,
+  `createdBy` varchar(40) NOT NULL,
+  `lastUpdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `lastUpdateBy` varchar(40) NOT NULL,
+  PRIMARY KEY (`cityId`),
+  KEY `countryId` (`countryId`),
+  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`countryId`) REFERENCES `country` (`countryId`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-27 21:51:43
